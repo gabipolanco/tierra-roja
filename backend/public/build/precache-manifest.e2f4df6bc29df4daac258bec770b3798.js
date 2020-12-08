8b07d53ea472050156baca4d8bd9bcf12d5ac83e
Tomi Scattiniclear <tomasscattini@gmail.com>
2020-12-08 16:07:39 -0500

    listo para deply

---
+++ backend/public/build/precache-manifest.e2f4df6bc29df4daac258bec770b3798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78ff56de03ccf16310338c802c74b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dcb50b9fefb4e86e021",
+    "url": "/static/css/2.9d6f32cf.chunk.css"
+  },
+  {
+    "revision": "0560a7687ecd78ab629c",
+    "url": "/static/css/main.daac1ec2.chunk.css"
+  },
+  {
+    "revision": "1dcb50b9fefb4e86e021",
+    "url": "/static/js/2.7c257e85.chunk.js"
+  },
+  {
+    "revision": "0560a7687ecd78ab629c",
+    "url": "/static/js/main.b92a523f.chunk.js"
+  },
+  {
+    "revision": "fb47f3c02db3ce888eb4",
+    "url": "/static/js/runtime-main.dc128e9d.js"
+  }
+]);
