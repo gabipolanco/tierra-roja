ec790a08008d7da07ec8ec2586a50a42a27f6017
Tomi Scattiniclear <tomasscattini@gmail.com>
2020-12-23 20:08:53 -0500

    gallery and portfolios responsive

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -2,48 +2,99 @@ import React, {useRef} from 'react'
 import styled from 'styled-components'
 
 const GalleryStyled = styled.div`
-& #carousel { 
+#carousel { 
   perspective: 1200px; 
   background: #FFF; 
   padding-top: 5%; 
   font-size:0; 
-  margin-bottom: 3rem;
+  margin-top: 20vh;
+  #spinner { 
+    transform-style: preserve-3d; 
+    height: 300px; 
+    transform-origin: 50% 50% -100vw; 
+    transition: 1s; 
+    img { 
+      width: 30vh; 
+      height: 20vh;
+      object-fit: cover;
+      position: absolute; 
+      left: 30%;
+      transform-origin: 50% 50% -45vh;
+      outline:1px solid transparent; 
+    }
+    img:nth-child(1) { transform:rotateY(0deg); }
+    img:nth-child(2) { transform: rotateY(-45deg); }
+    img:nth-child(3) { transform: rotateY(-90deg); }
+    img:nth-child(4) { transform: rotateY(-135deg); }
+    img:nth-child(5){ transform: rotateY(-180deg); }
+    img:nth-child(6){ transform: rotateY(-225deg); }
+    img:nth-child(7){ transform: rotateY(-270deg); }
+    img:nth-child(8){ transform: rotateY(-315deg); }
+  } 
 }
-& #spinner { 
-  transform-style: preserve-3d; 
-  height: 500px; 
-  transform-origin: 50% 50% -480px; 
-  transition: 1s; 
-} 
-& #spinner img { 
-  width: 40%; max-width: 480px; 
-  height: 500px;
-  object-fit: cover;
-  position: absolute; left: 30%;
-  transform-origin: 50% 50% -650px;
-  outline:1px solid transparent; 
-}
-& #spinner img:nth-child(1) { transform:rotateY(0deg); 
-}
-& #spinner img:nth-child(2) { transform: rotateY(-45deg); }
-& #spinner img:nth-child(3) { transform: rotateY(-90deg); }
-& #spinner img:nth-child(4) { transform: rotateY(-135deg); }
-& #spinner img:nth-child(5){ transform: rotateY(-180deg); }
-& #spinner img:nth-child(6){ transform: rotateY(-225deg); }
-& #spinner img:nth-child(7){ transform: rotateY(-270deg); }
-& #spinner img:nth-child(8){ transform: rotateY(-315deg); }
-& #carousel ~ span { 
-  color: #fff; 
+span { 
+  color: black; 
   margin: 5%; 
   display: inline-block; 
   text-decoration: none; 
   font-size: 2rem; 
   transition: 0.6s color; 
   position: relative; 
-  margin-top: -6rem; 
+  margin-top: 6rem; 
   border-bottom: none; 
-  line-height: 0; }
-& #carousel span:hover { color: #888; cursor: pointer; }
+  line-height: 0; 
+  &:hover { 
+    color: ${props => props.theme.color.brown}; 
+    cursor: pointer; 
+  }
+}
+span:first-of-type {
+  float: left;
+}
+span:last-of-type {
+  float: right;
+}
+p {
+  margin-top: 20vh;
+}
+@media ${props => props.theme.device.lgPhone} { 
+  #carousel #spinner { 
+    transform-origin: 50% 50% -40vw; 
+    }
+}
+@media ${props => props.theme.device.tablet} {
+  #carousel {
+    margin-top: 10vh;
+    #spinner {
+      transform-origin: 50% 50% -50vw;
+      img {
+        width: 36vh; 
+        height: 27vh;
+        transform-origin: 50% 50% -50vw;
+      }
+    }
+  }
+}
+@media ${props => props.theme.device.laptop} {
+  #carousel {
+    margin: 0 0 3rem 0;
+    #spinner {
+      height: 500px;
+      transform-origin: 50% 50% -650px;
+      img {
+        width: 35vw;
+        height: 30vw;
+        transform-origin: 50% 50% -50vw;
+      }
+    }
+  }
+  span {
+    margin-top: -6rem;
+  }
+  p {
+    margin-top: 20px;
+  }
+}
 `
 
 const Gallery = () => {
@@ -58,21 +109,20 @@ const Gallery = () => {
 
     return (
         <GalleryStyled className="page">
-
-                <div id="carousel">
-                <figure ref={spinnerRef} id="spinner">
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu_pjnuuh.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu2_bmjynf.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu3_mebxpt.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu5_scczh5.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528211/tierra-roja/Imagenes%20pagina/alu6_wwbnuw.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528211/tierra-roja/Imagenes%20pagina/alu7_yvq3it.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528212/tierra-roja/Imagenes%20pagina/alu8_kain6h.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                    <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528212/tierra-roja/Imagenes%20pagina/alu9_ggx2up.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
-                </figure>
-                </div>
-                <span style={{color: "black", cursor: "pointer", float: "left"}} className="ss-icon" onClick={() => galleryspin('-')}>&lt;</span>
-                <span style={{color: "black", cursor: "pointer", float:"right"}} className="ss-icon" onClick={() => galleryspin('')}>&gt;</span>
+            <div id="carousel">
+              <figure ref={spinnerRef} id="spinner">
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu_pjnuuh.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu2_bmjynf.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu3_mebxpt.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528122/tierra-roja/Imagenes%20pagina/alu5_scczh5.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528211/tierra-roja/Imagenes%20pagina/alu6_wwbnuw.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528211/tierra-roja/Imagenes%20pagina/alu7_yvq3it.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528212/tierra-roja/Imagenes%20pagina/alu8_kain6h.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+                  <img src="https://res.cloudinary.com/tomiscattini/image/upload/v1607528212/tierra-roja/Imagenes%20pagina/alu9_ggx2up.jpg" alt="Trabajo de Aluminé Fernández Rodriguez" />
+              </figure>
+            </div>
+            <span onClick={() => galleryspin('-')}>&lt;</span>
+            <span onClick={() => galleryspin('')}>&gt;</span>
             <p>Artista invitada: Aluminé Fernández Rodriguez</p>
         </GalleryStyled>
     )
--- frontend/src/pages/Portfolios.js
+++ frontend/src/pages/Portfolios.js
@@ -7,49 +7,61 @@ const PortfoliosStyled = styled.div`
 position: relative;
 height: 100vh;
 overflow-y: scroll;
-& .title {
-position: fixed;
-bottom: 0;
-font-size: 7vw;
-margin: 0 13vw;
-font-weight: bold;
-text-transform: uppercase;
-font-family: ${props => props.theme.font.primary};
+.title {
+    position: fixed;
+    bottom: 0;
+    margin: 10px calc(50vw - 140px);
+    h2 {
+        font-size: 2rem;
+        font-weight: bold;
+        text-transform: uppercase;
+        font-family: ${props => props.theme.font.primary};
+    }
 }
-& .names-container {
-    transform: rotate(-90deg);
-}
-& .names {
-  height: 200px;
-  max-width: 100vw;
-  margin: 0 auto;
-  position: relative;
-  overflow: hidden;
-  transform: translate3d(0, 0, 0);
-}
-& .names>div {
-  height: 50px;
-  width: 1000px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  font-size: 3rem;
-  font-family: ${props => props.theme.font.primary};
-  text-align: right;
-  transform: translate3d(0, 0, 0);
-}
-& .mover {
-    animation: moveSlideshow 20s linear infinite;
-}
-& .title h2 {
-    margin-bottom: -60px;
+.names-container {
+    margin: 140px 0 0 50px;
+    .mover {
+        font-family: ${props => props.theme.font.primary};
+        font-size: 1.5rem;
+    }
 }
 
 @keyframes moveSlideshow {
   100% { 
-    transform: translateX(-66.6666%);  
+    transform: translateX(-100%);  
   }
+}
+
+@media ${props => props.theme.device.tablet} {
+    .title {
+        margin: 0 19vw;
+        h2 {
+            font-size: 8vw;
+            margin-bottom: -30px;
+        }
+    }
+    .names-container {
+        transform: rotate(-90deg);
+        .names {
+            height: 200px;
+            width: 100vh;
+            margin: 0 auto;
+            position: relative;
+            overflow: hidden;
+            transform: translate3d(0, 0, 0);
+            .mover {
+                height: 50px;
+                width: 1000px;
+                position: absolute;
+                height: 100%;
+                font-size: 3rem;
+                
+                text-align: right;
+                transform: translate3d(0, 0, 0);
+                animation: moveSlideshow 20s linear infinite;
+            }
+        }
+    }
 }
 `
 
@@ -58,18 +70,18 @@ const Portfolios = () => {
 
     return (
         <PortfoliosStyled className="page">
-        <div className="names-container">
-            
-            {allArtists && 
-                allArtists.map((art) => {
-                    return <div className="names">
-                    <div className="mover">
-                        <Link to={`/portfolio/${art._id}`}><p>{art.name}</p></Link>
-                    </div>
-                    </div>
-                })
-            }
-        </div>
+            <div className="names-container">
+                
+                {allArtists && 
+                    allArtists.map((art) => {
+                        return <div className="names">
+                        <div className="mover">
+                            <Link to={`/portfolio/${art._id}`}><p>{art.name}</p></Link>
+                        </div>
+                        </div>
+                    })
+                }
+            </div>
             
             <div className="animate__animated animate__bounceInLeft title"><h2>Artistas misioneras</h2></div>
         </PortfoliosStyled>
