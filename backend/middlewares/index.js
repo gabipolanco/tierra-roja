5eb5d716887225ad736d775d45c83447e87bba8c
tomasscattini <tomasscattini@gmail.com>
2021-02-08 12:08:00 -0500

    middlewares refactored

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,19 +1,11 @@
 exports.catchErrs = ctl => (req, res, next) => ctl(req, res).catch(next)
 
 exports.isAuth = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next()
-  } else {
-    res.status(403).json({
-      message: 'You must login'
-    })
-  }
+  if (req.isAuthenticated()) return next()
+  return res.status(403).json({ message: 'You must login'})
 }
 
 exports.checkRole = role => (req, res, next) => {
-  if (role.includes(req.user.role)) {
-      return next()
-  } else {
-      res.status(400).json({message: "You have to be an artist to get here"})
-  }
+  if (role.includes(req.user.role)) return next()
+  return res.status(400).json({message: "You have to be an artist to get here"})
 }
